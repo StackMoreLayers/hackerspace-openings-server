a061bf547badac43438f869264ab4dd2d97ef149
Nikita Marchant <nikita.marchant@gmail.com>
2017-10-10 18:52:11 +0200

    Doctrings

--- yolo.py
+++ yolo.py
@@ -6,12 +6,23 @@ import json
 
 class Yolo(BaseModel):
     def __init__(self):
+        """
+        Init is called once at the start of the server/thread and loads the model
+        from it's cache directory (self.get_cache_dir())
+        """
         with open(os.path.join(self.get_cache_dir(), "data.json"), 'w') as fd:
             self.MAGIC = json.reads(fd.read())["random_number"]
 
     def train(self):
+        """
+        Train will be called outside of the request/response cycle and has
+        to save the model in it's cache dir (self.get_cache_dir())
+        """
         with open(os.path.join(self.get_cache_dir(), "data.json"), 'w') as fd:
             fd.write(json.dumps({"random_number": 4}))
 
     def predict(self, past, lenght):
+        """
+        Predict is called at every query
+        """
         return np.random.rand(lenght) * self.MAGIC
